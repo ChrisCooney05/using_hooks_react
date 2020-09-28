0cbbe980ae68f9d25773c85e3800c083a82c46ad
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-28 11:45:01 +0100

    Able to use useEffect to replace componentDidMount, async await function used to make fetch call

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { options } from "../constants";
 
 const Counter = () => {
   const [count, updateCount] = useState(0);
+  const [name, updateName] = useState("");
 
   const addOne = () => {
     updateCount(count + 1);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.github.com/users/ChrisCooney05",
+        options
+      );
+      const data = await response.json();
+      updateName(data.name);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>
+        {name} has counted {count} many times
+      </h1>
       <br />
       <button onClick={addOne}>Add One</button>
     </div>
